c8df1f26880eff201cda664ddf35098c835d0be8
BekirLee <nihadbekirli8@gmail.com>
2024-04-16 15:46:56 +0400

    add new codes updayed code for project

--- vite-project/src/layouts/main/sidebar/account/index.jsx
+++ vite-project/src/layouts/main/sidebar/account/index.jsx
@@ -35,6 +35,9 @@ export default function Account() {
                         <More />
 
                     </Popover.Panel>
+                    <svg viewBox="0 0 24 24" aria-hidden="true" className="">
+                        <path d="M22 17H2L12 6l10 11z"></path>
+                    </svg>
                 </Transition>
             </Popover>
 
