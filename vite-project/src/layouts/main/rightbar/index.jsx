64bb5d7f30e843164e6602815b972605e420bc4e
BekirLee <nihadbekirli8@gmail.com>
2024-04-20 01:16:05 +0400

    add new codes for rightbar

--- vite-project/src/layouts/main/rightbar/index.jsx
+++ vite-project/src/layouts/main/rightbar/index.jsx
@@ -3,7 +3,7 @@ import Search from "./search";
 export default function RightBar() {
     return (
         <>
-            <aside className="w-[350px] mr-2.5 ">
+            <aside className="w-[350px] mr-2.5 ml-[30px]">
                 <Search />
             </aside>
         </>
--- vite-project/src/layouts/main/rightbar/search/index.jsx
+++ vite-project/src/layouts/main/rightbar/search/index.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
+
 export default function Search() {
+    
+    const [query, setQuery] = useState('');
+
     return (
-        <div className="min-h-[32px] h-[53px] mb-3">
-            <label className="h-[43px] rounded-full bg-[#202327] w-full block">
-                search
+        <div className="min-h-[32px] h-[54px] mb-3 flex items-center">
+            <label className="h-[43px] rounded-full bg-[#202327] w-full block border border-transparent relative flex items-center">
+                <div className="w-[64px] h-full flex items-center justify-center">
+                    <svg viewBox="0 0 24 24" aria-hidden="true" className="min-w-[32px] h-[18.75px] pl-[4px]" >
+                        <path fill="#657786" d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
+                    </svg>
+                </div>
+                <input type="text" placeholder="Search" className="w-full h-full bg-transparent rounded-full outline-none pl-[1px] text-[15px]" />
+
             </label>
         </div>
     )
