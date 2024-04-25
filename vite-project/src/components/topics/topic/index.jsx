404ddce003706b5fcb95eb338753e4fc6f754e39
BekirLee <nihadbekirli8@gmail.com>
2024-04-25 21:34:00 +0400

    add new changes for the site last time

--- vite-project/src/components/topics/topic/index.jsx
+++ vite-project/src/components/topics/topic/index.jsx
@@ -5,7 +5,7 @@ export default function Topic({ item }) {
     return (
         <Link
             to='/'
-            className="py-3 px-4 transition-colors flex items-center justify-between"
+            className="py-3 transition-colors flex justify-between"
         >
             <div className="">
 
@@ -15,19 +15,22 @@ export default function Topic({ item }) {
                     </span>
                 </div>
                 <div className="text-[#e7e9ea] font-bold">
-                    <span>
+                    <span className="font-sans">
                         {
                             item.topic.type == 'tag' ? '#' + item.topic.value : item.topic.value
                         }
                     </span>
                 </div>
+                <div className="text-[#71767C] text-[13px] leading-4 font-sans">
+                    {item.postCount ? item.postCount.toLocaleString() + 'K posts' : item.postCount}
+                </div>
             </div>
-            <div className="">
-                <svg viewBox="0 0 24 24" aria-hidden="true" width={18.75} height={18.75} className="">
-                    <path fill="#fff" d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z">
+            <button className="rounded-full p-2 group hover:bg-[#1d9bf01a] w-[34.75px] h-[34.75px] text-center hover:text-[#1d9bf0]">
+                <svg viewBox="0 0 24 24" aria-hidden="true" width={18.75} height={18.75} >
+                    <path fill="currentColor" className="hover:fill-[#3b82f680]" d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z">
                     </path>
                 </svg>
-            </div>
+            </button>
         </Link >
     )
 }
