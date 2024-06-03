d99d458b06df3d5b594635e8e55b9f4cbee40383
BekirLee <nihadbekirli8@gmail.com>
2024-06-03 23:52:20 +0400

    changes before set all colors dinamicly

--- vite-project/src/utilits/consts.jsx
+++ vite-project/src/utilits/consts.jsx
@@ -148,3 +148,12 @@ export const topics = [
         postCount: 12803
     }
 ]
+
+export const colors = [
+    '#1d9bf0',
+    '#ffd400',
+    '#f91880',
+    '#7856ff',
+    '#ff7a00',
+    '#00ba7c'
+]
