a045c69afd94f971d955045be9e1dbce76e3e35c
BekirLee <nihadbekirli8@gmail.com>
2024-08-27 00:57:50 +0400

    infinte timeline window scrool

--- vite-project/src/pages/home/for-you/index.jsx
+++ vite-project/src/pages/home/for-you/index.jsx
@@ -1,21 +1,32 @@
-import Post from "~/components/post"
-import { posts  } from "~/mock/posts"
+import Post from "~/components/post";
+import { posts as postData } from "~/mock/posts";
 import { WindowVirtualizer } from "virtua";
-import { useState } from "react"
-
+import { useState } from "react";
 
 export default function ForYou() {
-    // const [posts, setPosts] = useState(postData)
+    const [posts, setPosts] = useState(postData.concat(postData, postData));
 
     return (
         <WindowVirtualizer
             onRangeChange={(start, end) => {
-                console.log('star', start)
-                console.log('end', end)
+                console.log("start", start);
+                console.log("end", end);
+                if (end + 1 == posts.length) {
+                    setPosts(prev => [...prev, ...postData])
+                }
             }}>
-            <>
-                {posts.from(({ length: 1000 }).map((_, post) => <Post post={post} key={post.id + key} />))}
-            </>
+            {posts.map((post, key) => (
+                <Post key={post.id + key} post={post} />
+            ))}
         </WindowVirtualizer>
-    )
+    );
 }
+{/* <WindowVirtualizer
+    onRangeChange={(start, end) => {
+        console.log('star', start)
+        console.log('end', end)
+    }}>
+    <>
+        {posts.from(({ length: 1000 }).map((_, post) => <Post post={post} key={post.id + key} />))}
+    </>
+</WindowVirtualizer> */}
